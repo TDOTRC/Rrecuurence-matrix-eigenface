b2b84db6d992a49fa285d70cbe4247c9ef4a9bac
TDOTRC <13146715796@163.com>
2025-12-01 13:44:37 +0800

    Update OneGraphAnalyse1.m

--- OneGraphAnalyse1.m
+++ OneGraphAnalyse1.m
@@ -1,5 +1,6 @@
 %% cauculate the PCA based on one matrix
 %% lorentz stability under approach 2
+load("E:\研究生\论文1\论文1\数据\lor距离\data4.mat")
 Size=2000;
 Col=15;
 Vol=20;
@@ -84,6 +85,111 @@ for i=1:10
     subplot(2,5,i)
     imagesc(log(abs(norm(:,:,i))))
 end
+figure(2)
+for j=1:10
+    subplot(2,5,j)
+    scatter([1:15],log(eVal(:,j)),4,"red","filled")
+end
+%% sin random
+Size=2000;
+Col=15;
+Vol=20;
+distance=zeros(Size,Size,Vol);
+pca=zeros(Size,Col,Vol);
+t=[0.01:0.01:20];
+z=zeros(Vol,Size);
+for i=1:Vol
+ f=rand(1);
+ f=f+0.5;
+z(i,:)=sin(2*pi*f*t);
+end
+for i=1:Vol
+    for j=1:Size
+        for k=1:Size
+            distance(j,k,i)=abs(z(i,k)-z(i,j));
+        end
+    end
+    i
+end
+Cov=zeros(Size,Size);
+eVec=zeros(Size,Col,Vol);
+eVal=zeros(Col,Vol);
+for i=1:20
+    Cov=distance(:,:,i)*distance(:,:,i)';
+    [eVector,eValue]=eig(Cov);
+    for j=1:Col
+        eVec(:,j,i)=eVector(:,Size-(j-1));
+    end
+    mid=flip(diag(eValue));
+    eVal(:,i)=mid(1:15)';
+    i
+end
+%verify thewhether it is normal
+norm=zeros(Col,Col,Vol);
+for i=1:Vol
+        norm(:,:,i)=eVec(:,:,i)'*eVec(:,:,5);
+end
+figure(1)
+for i=1:10
+    subplot(2,5,i)
+    imagesc(abs(norm(:,:,i)))
+end
+figure(2)
+for j=1:10
+    subplot(2,5,j)
+    scatter([1:15],log(eVal(:,j)),4,"red","filled")
+end
+%% analyses by tiled matrix
+Size=2000;
+Col=15;
+Vol=20;
+distance=zeros(Size,Size,Vol);
+pca=zeros(Size,Col,Vol);
+t=[0.01:0.01:20];
+z=zeros(Vol,Size);
+for i=1:Vol
+ f=rand(1);
+ f=f+0.5;
+z(i,:)=sin(2*pi*f*t);
+end
+for i=1:Vol
+    for j=1:Size
+        for k=1:Size
+            distance(j,k,i)=abs(z(i,k)-z(i,j));
+        end
+    end
+    i
+end
+Cov=zeros(Size/2,Size/2);
+eVec=zeros(Size/2,Col,Vol);
+eVal=zeros(Col,Vol);
+distanceDone=zeros(Size/2,Size/2,Vol);
+for i=1:Vol
+for j=1:Size/2
+    distanceDone(j,:,i)=distance(j,j:j+Size/2-1,i);
+end
+end
+for i=1:20
+    Cov=distanceDone(:,:,i)*distanceDone(:,:,i)';
+    [eVector,eValue]=eig(Cov);
+    for j=1:Col
+        eVec(:,j,i)=eVector(:,Size/2-(j-1));
+    end
+    mid=flip(diag(eValue));
+    eVal(:,i)=mid(1:15)';
+    i
+end
+%verify thewhether it is normal
+norm=zeros(Col,Col,Vol);
+for i=1:Vol
+        norm(:,:,i)=eVec(:,:,i)'*eVec(:,:,5);
+end
+figure(1)
+for i=1:10
+    subplot(2,5,i)
+    imagesc(abs(norm(:,:,i)))
+end
+colorbar
 figure(2)
 for j=1:10
     subplot(2,5,j)
